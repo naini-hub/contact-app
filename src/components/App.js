bc69e3a5ce94e0c216f9ff6ccef160dfc5bc034c
gaurav nainwal <gauravnainwal@gauravs-MacBook-Air.local>
2025-07-08 00:33:37 +0530

    added contact form functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React , {useState} from 'react';
+import React , {useState ,useEffect} from 'react';
 import './App.css';
 import Header from './header';
 import AddContact from './AddContact';
@@ -6,11 +6,26 @@ import ContactList from './ContactList';
 
 
 function App() {
+  const LOCAL_STORAGE_KEY= "contacts";
   const [contacts, setContacts] = useState([]);
+  const addContactHandler = (contact) => {
+    console.log("adding contact:",contact);
+    setContacts([...contacts, contact]);
+  }
+
+  useEffect(() => {
+    const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(retrieveContacts) setContacts(retrieveContacts);
+  },[]);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts))
+  },[contacts]);
+
   return (
-    <div className='ui contain'>
+    <div className='ui container'>
       <Header/>
-      <AddContact/>
+      <AddContact addContactHandler={addContactHandler}/>
       <ContactList contacts={contacts}/>
       
     </div>
